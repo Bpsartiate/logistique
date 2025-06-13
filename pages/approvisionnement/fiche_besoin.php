7730a79fad93707bbae34d1c92d23bd87a8cfdd6
johanmuyali <johanmuyali@gmail.com>
2025-06-13 12:50:48 +0200

    Add 4 pages

---
+++ pages/approvisionnement/fiche_besoin.php
@@ -0,0 +1,19 @@
+<!-- Modal -->
+<div class="modal fade" id="ficheFournisseur" tabindex="-1" aria-labelledby="ficheFournisseurLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="ficheFournisseurLabel">Modal title</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <div class="view_approvisionnement_details">
+            
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
